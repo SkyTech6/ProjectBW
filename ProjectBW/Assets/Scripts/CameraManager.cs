012a4ea43b9b3ff467dd1f80f2df3558d0180002
SkyTech6 <seth_rebirth@yahoo.com>
2016-09-25 20:23:21 -0700

    0.0.0.8
    Camera Update

---
+++ ProjectBW/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	public float modifier;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Camera.main.orthographicSize = modifier / Screen.width * Screen.height;
+	}
+}
