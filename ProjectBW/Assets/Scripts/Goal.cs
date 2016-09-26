0948dabd99fb6d2f9ce61b102a38d02817352a88
SkyTech6 <seth_rebirth@yahoo.com>
2016-09-25 19:47:05 -0700

    0.0.0.7
    Added a Goal and load next level.

---
+++ ProjectBW/Assets/Scripts/Goal.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+	SpriteRenderer sr;
+	PlayerController pc;
+
+	public Object sceneToLoad;
+
+	public string playerColorMatch = "b";
+
+	// Use this for initialization
+	void Start () 
+	{
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+		pc = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+
+		if (playerColorMatch == "b")
+			sr.color = GameManager.cBlack;
+		else if (playerColorMatch == "w")
+			sr.color = GameManager.cWhite;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D()
+	{
+		if (pc.color == playerColorMatch) {
+			StartCoroutine (NextLevel ());
+		}
+	}
+
+	IEnumerator NextLevel()
+	{
+		yield return new WaitForSeconds (1f);
+		SceneManager.LoadScene(sceneToLoad.name);
+	}
+}
